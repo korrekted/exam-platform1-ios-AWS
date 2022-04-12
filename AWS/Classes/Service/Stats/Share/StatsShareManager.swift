ef19268cd7ea812e3b9b1b45186e3ae9dafd0f4c
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-04-12 13:01:58 +0100

    config

--- AWS/Classes/Service/Stats/Share/StatsShareManager.swift
+++ AWS/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.aws.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.aws.widget")
     }
     
     @discardableResult
