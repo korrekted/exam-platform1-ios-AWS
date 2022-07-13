c794e13b42fde022d6082960e9c7efa235a85c16
akonst17 <akonst17@gmail.com>
2022-07-13 13:40:21 +0300

    новые фичи из Nursing; build 15

--- AWS/Classes/Misc/GlobalDefinitions.swift
+++ AWS/Classes/Misc/GlobalDefinitions.swift
@@ -6,17 +6,18 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams2.quantresear.ch"
+    static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "hQFvAU5TpF65gGhj"
 
     static let sdkDomainUrl = "https://analytics.korrekted.com"
     static let sdkApiKey = "Cpj8uw8AYgDAeDed"
 
-    static let applicationTag = "EXAM_AWS"
+    static let applicationTag = "EXAM_MILITARY"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
+    static let appStoreUrl = "https://apps.apple.com/app/training-for-aws/id1576524128"
     static let appleAppID = "1576524128"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
