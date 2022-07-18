da1c3f9375813d9962cfc3975a770a93436d6672
akonst17 <akonst17@gmail.com>
2022-07-18 11:27:15 +0300

    fix amplitude log; build 16

--- AWS/Classes/Misc/GlobalDefinitions.swift
+++ AWS/Classes/Misc/GlobalDefinitions.swift
@@ -12,7 +12,7 @@ final class GlobalDefinitions {
     static let sdkDomainUrl = "https://analytics.korrekted.com"
     static let sdkApiKey = "Cpj8uw8AYgDAeDed"
 
-    static let applicationTag = "EXAM_MILITARY"
+    static let applicationTag = "EXAM_AWS"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
