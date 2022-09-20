902c15dd2b44c1a9919fed1f457a3d3f79eccd15
akonst17 <akonst17@gmail.com>
2022-09-20 19:00:01 +0300

    интеграция sdk 2.0; новые урлы; build 24

--- AWS/Classes/Misc/GlobalDefinitions.swift
+++ AWS/Classes/Misc/GlobalDefinitions.swift
@@ -14,16 +14,16 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
     
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
     static let sdkApiKey = "Cpj8uw8AYgDAeDed"
     
-    static let otterScaleHost = "https://api.korrekted.com"
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
     static let otterScaleApiKey = "m7bChJyZPxvD80nh"
 
     static let appStoreUrl = "https://apps.apple.com/app/training-for-aws/id1576524128"
     static let appleAppID = "1576524128"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
